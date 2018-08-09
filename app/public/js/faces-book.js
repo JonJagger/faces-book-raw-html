84e665c5a2d6aae18e3b19724bd7b243578504ee
Jon Jagger <jon@jaggersoft.com>
2018-08-09 08:19:14 +0100

    used CSS to make the faces images small and rounded

--- app/public/js/faces-book.js
+++ app/public/js/faces-book.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
 
   // ready for Javascript which will run when the html is loaded
 
+  // Add  <b>Hello</b> after <faces>
+
+  //const faces = $('faces');
+  //const add = $('<b>').html('Hello');
+  //faces.append(add);
+
 });
