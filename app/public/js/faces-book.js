cb32d8c7c60a9c64086057aacd7df2154c1fcad6
Jon Jagger <jon@jaggersoft.com>
2018-08-09 10:27:15 +0100

    changed png files to jpg files and created all img elements in JS

--- app/public/js/faces-book.js
+++ app/public/js/faces-book.js
@@ -1,15 +1,48 @@
 $(() => {
 
-  const faces = $('faces');
-
   const makeFace = function(name) {
-    return $('<img>', {
+    return $("<img>", {
       title:name,
         src:`img/2018/${name}.jpg`,
       class:"face"
     });
   };
 
-  faces.append(makeFace("Thiru"));
+  const names = [
+    "Akshat",
+    "Karthika",
+    "Sanjana",
+    "Mahidher",
+    "Akhil",
+    "Keerthana",
+    "RVishnuPriya",
+    "Anushree",
+    "Mariah",
+    "Aishu",
+    "Rishi",
+    "Jon",
+    "Pavithrann",
+    "Sindhu",
+    "Rishi",
+    "Supriya",
+    "Shruti",
+    "Santhosh",
+    "Supriya",
+    "Gayatri",
+    "Vishnu",
+    "Varsha",
+    "John",
+    "Ameya",
+    "Sudeep",
+    "Janani",
+    "Thiru",
+    "Thamizh"
+  ];
+
+  const faces = $("faces");
+
+  names.forEach(function(name) {
+    faces.append(makeFace(name));
+  });
 
 });
