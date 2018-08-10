c2a48817616b5922147fec3efb907afb37bb95fe
Jon Jagger <jon@jaggersoft.com>
2018-08-10 04:40:24 +0100

    reset faces-book.js to empty; reset app/public/index.html to full name list with no duplicated and all as .jpg

--- app/public/js/faces-book.js
+++ app/public/js/faces-book.js
@@ -1,48 +1,5 @@
-$(() => {
+$(document).ready(function() {
 
-  const makeFace = function(name) {
-    return $("<img>", {
-      title:name,
-        src:`img/2018/${name}.jpg`,
-      class:"face"
-    });
-  };
-
-  const names = [
-    "Akshat",
-    "Karthika",
-    "Sanjana",
-    "Mahidher",
-    "Akhil",
-    "Keerthana",
-    "RVishnuPriya",
-    "Anushree",
-    "Mariah",
-    "Aishu",
-    "Rishi",
-    "Jon",
-    "Pavithrann",
-    "Sindhu",
-    "Rishi",
-    "Supriya",
-    "Shruti",
-    "Santhosh",
-    "Supriya",
-    "Gayatri",
-    "Vishnu",
-    "Varsha",
-    "John",
-    "Ameya",
-    "Sudeep",
-    "Janani",
-    "Thiru",
-    "Thamizh"
-  ];
-
-  const faces = $("faces");
-
-  names.forEach(function(name) {
-    faces.append(makeFace(name));
-  });
+  // ready for Javascript which will run when the html is loaded
 
 });
